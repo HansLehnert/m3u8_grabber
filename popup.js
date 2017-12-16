55df85af1c11813de5cd2f9eba8cd7f632113df6
Hans <hans.lehnert@gmail.com>
2017-12-16 18:00:58 -0300

    Added some simple styling

--- popup.js
+++ popup.js
@@ -3,18 +3,18 @@ document.addEventListener("DOMContentLoaded", function () {
 		var active_tab = tabs[0];
 
 		chrome.tabs.sendMessage(active_tab.id, {}, function (reply) {
-			document.body.appendChild(document.createElement("p")).innerHTML = "Available sources";
 			//document.body.appendChild(document.createElement("div")).innerHTML = JSON.stringify(reply, null, 2);
 
 			for (var i in reply.sources) {
-				var entry = document.createElement("a");
-				entry.setAttribute("href", reply.sources[i].name);
-				entry.innerHTML = reply.sources[i].name.split("?")[0].split("/").slice(-1)[0];
+				var url = reply.sources[i].name;
+
+				var entry = document.createElement("div");
+				entry.classList.add("entry");
+				entry.innerHTML = url.split("?")[0].split("/").slice(-1)[0];
 				entry.onclick = function () {
 					chrome.tabs.create({active: true, url: reply.sources[i].name});
 				}
 				document.body.appendChild(entry);
-				document.body.appendChild(document.createElement("br"));
 			}
 		});
 	});
